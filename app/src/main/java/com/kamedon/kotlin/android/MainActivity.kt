04d1dde2bdf2e80b428118ada2b01cf4aa751a14
Kamei Hidetoshi <h_kamei@menue.co.jp>
2015-10-27 18:23:34 +0900

    onclickを設定

--- app/src/main/java/com/kamedon/kotlin/android/MainActivity.kt
+++ app/src/main/java/com/kamedon/kotlin/android/MainActivity.kt
@@ -1,18 +1,15 @@
 package com.kamedon.kotlin.android
 
 import android.os.Bundle
-import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById(R.id.btn_send).setOnClickListener { view -> Log.d("MainActivity", "click: btn_send") };
     }
 }
