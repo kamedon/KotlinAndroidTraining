175326f51c24e5f85186ccac0baa8ed5d30110cc
Kamei Hidetoshi <h_kamei@menue.co.jp>
2015-10-28 10:36:52 +0900

    setting Kotlin Android Extensions

--- app/src/main/java/com/kamedon/kotlin/android/MainActivity.kt
+++ app/src/main/java/com/kamedon/kotlin/android/MainActivity.kt
@@ -3,13 +3,13 @@ package com.kamedon.kotlin.android
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import kotlinx.android.synthetic.activity_main.btn_send
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById(R.id.btn_send).setOnClickListener { view -> Log.d("MainActivity", "click: btn_send") };
+        btn_send.setOnClickListener({ view -> Log.d("MainActivity", "click: btn_send") })
     }
 }
